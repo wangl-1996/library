b7b9ec35321bab2d2147059b041bf8d40561e5be
wanglei <1992454838@qq.com>
2018-07-30 14:57:38 +0800

    微信支付

---
+++ weixin.php
@@ -0,0 +1,18 @@
+<?php
+
+# weixin.php
+# 2018 07 30 下午12:04
+# 王磊
+# 1992454838@qq.com
+# 微信支付demo
+
+define('ON_WEIXIN', false !== stripos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger'));
+
+$url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx60ef890ebfb97045&redirect_uri='.urlencode('www.guojf.com/weixin.php').'&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
+
+if (!isset($_GET['code'])) {
+    header('location:'.$url);
+    die;
+}
+
+var_dump($_GET);
