462f2ff5ae400f13210c96ce74dc0141c451f1e7
wanglei <1992454838@qq.com>
2018-07-31 09:46:26 +0800

    通知

--- notify.php
+++ notify.php
@@ -5,10 +5,10 @@
 # 王磊
 # 1992454838@qq.com
 
-file_put_contents('./notify', "quit:\r\n".json_encode($_GET));
+file_put_contents('./get', "notify\r\n".json_encode($_GET));
 
-file_put_contents('./notify', "quit\r\n".json_encode($_POST));
+file_put_contents('./post', "notify\r\n".json_encode($_POST));
 
-file_put_contents('./notify', "quit\r\n".file_get_contents('php://input'));
+file_put_contents('./input', "notify\r\n".file_get_contents('php://input'));
 
 die('<xml><return_code><![CDATA[SUCCESS]]></return_code><return_msg><![CDATA[OK]]></return_msg></xml>');
